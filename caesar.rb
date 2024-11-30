7d7df7c09012222de09192c5fa83491bc6f5abd8
Necromancy501 <94140246+Necromancy501@users.noreply.github.com.>
2024-11-30 11:39:42 +0800

    Fii hfjxfw_hdumjw kzshynts

---
+++ caesar.rb
@@ -0,0 +1,42 @@
+def cycle_through(floor, ceiling, index, shift)
+  negative_flag = false
+  if shift.negative?
+    shift = shift.abs
+    negative_flag = true
+  end
+
+  while(shift > 0)
+    if ((index == ceiling) and (!negative_flag))
+      index = floor
+    elsif (index == floor) and (negative_flag)
+      index = ceiling
+    else
+      if(negative_flag)
+        index-= 1
+      else
+        index+= 1
+      end
+    end
+
+    shift-= 1
+    
+  end
+  index
+end
+
+def caesar_cipher(message, shift=0)
+  new_msg = message.chars.map do |c|
+    ascii_code = c.ord
+    case ascii_code
+    when 48..57
+      cycle_through(48, 57, ascii_code, shift).chr
+    when 65..90
+      cycle_through(65, 90, ascii_code, shift).chr
+    when 97..122
+      cycle_through(97, 122, ascii_code, shift).chr
+    else
+      c
+    end
+  end
+  new_msg.join
+end
